acd207aa33e3f571ee1ac3b6f12706f795433a56
21wh5a6606 <93035370+21wh5a6606@users.noreply.github.com>
2022-05-26 16:09:40 +0530

    Add files via upload

---
+++ pipes.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+int main(){
+int pd1[2],pd2[2];
+char s1[] ="BVRIT" , s2[] = "Hyderabad",s3[100],s4[100];
+pipe(pd1);
+pipe(pd2);
+int p  = fork();
+if(p > 0){
+close(pd1[0]);
+write(pd1[1],s1,20);
+close(pd1[1]);
+wait(NULL);
+close(pd2[1]);
+read (pd2[0],s4,20);
+printf("string 2:%s\n",s4);
+close(pd2[0]);
+}
+else if(p == 0)
+{
+close(pd1[1]);
+read(pd1[0],s3,20);
+printf("string 1:%s\n",s3);
+close(pd1[0]);
+close(pd2[0]);
+write(pd2[1],s2,25);
+close(pd2[1]);
+exit(0);
+}
+}
