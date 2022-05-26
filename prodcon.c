01e37f0df4de664bb2c0f8e6157f12f3c929f4ce
21wh5a6606 <93035370+21wh5a6606@users.noreply.github.com>
2022-05-26 15:50:42 +0530

    Add files via upload

---
+++ prodcon.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+int full = 0,empty = 5,mutex = 1,x =0;
+int wait(int s)
+{
+s--;
+return s;
+}
+
+int signal(int s)
+{
+s++;
+return s;
+}
+void producer(){
+mutex = wait(mutex);
+full = signal(full);
+empty = wait(empty);
+x++;
+printf("producer produces the item %d",x);
+mutex = signal(mutex);
+}
+void consumer(){
+	mutex = wait(mutex);
+	full = wait(full);
+	empty = signal(empty);
+printf("consumer consumes item %d",x);
+x--;
+mutex = signal(mutex);
+}
+int main(){
+int ch;
+printf("1.producer\n 2.consumer\n 3.exit\n");
+while(1){
+printf("enter the choice\n");
+scanf("%d",&ch);
+switch(ch){
+case 1: if(mutex == 1 && (empty != 0))
+		producer();
+		else
+		printf("buffer is full");
+		break;
+case 2:if(mutex == 1 && (full != 0))
+		consumer();
+		else
+		printf("buffer is empty");
+		break;
+case 3:exit(0);
+}
+}
+}
+
